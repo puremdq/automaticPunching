b8c28168651174d2b4539d06720ea719b6cd227e
puremdq <puremdq@gmail.com>
2020-06-26 23:38:10 +0800

    创建入口执行文件

--- execPunch.py
+++ execPunch.py
@@ -1,3 +1,7 @@
-import healthPunch.autoPunch 
+import healthPunch.autoPunch as autoPunch
+import asst.asstPunch as asstPunch
 
-print(autoPunch.startPunch('BT7604','BT7604'))
+
+key = "MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCgZmNj7QvhbpgdqxN7ZCR+r874KZb/qRvlHRieJJREH+i5/hPbpPH5KheEFxoo7nyAkPIcQYPshHvC4UJBe1HrHjdhjFnMA967aebBtioXBOB0qR4ql0DtWA0PrJWtDABeTpPXedqmzMcYIxr1Wq/viIPsjCHRiyRx6mhYqT5P6wIDAQAB"
+print(autoPunch.startPunch('BT7604', 'BT7604'))
+print(asstPunch.startPunch('18580935926', '991458902', key))
