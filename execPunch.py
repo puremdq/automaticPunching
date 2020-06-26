8a5a0c5b254ae1923bdbe5db2c66b6748f3e4c46
puremdq <puremdq@gmail.com>
2020-06-27 01:59:11 +0800

    Update execPunch.py

--- execPunch.py
+++ execPunch.py
@@ -4,15 +4,6 @@ import time
 import sys
 key = "MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCgZmNj7QvhbpgdqxN7ZCR+r874KZb/qRvlHRieJJREH+i5/hPbpPH5KheEFxoo7nyAkPIcQYPshHvC4UJBe1HrHjdhjFnMA967aebBtioXBOB0qR4ql0DtWA0PrJWtDABeTpPXedqmzMcYIxr1Wq/viIPsjCHRiyRx6mhYqT5P6wIDAQAB"
 
-
-def startFromFile(filename):
-    accounts = open(filename, 'r', encoding='utf-8').read().split('\n')
-    for account in accounts:
-        if account.startswith('#') or len(account) < 1:
-            continue
-        start(account)
-
-
 def start(account):
     account = account.split(" ")
     if '2'.endswith(account[2]):
@@ -22,5 +13,12 @@ def start(account):
         print(time.strftime("%Y-%m-%d-%H:%M:%S", time.localtime()) +
               " 执行用户:" + account[0] + " 健康打卡 "+str(autoPunch.startPunch('BT7604', 'BT7604')))
 
+
+def startFromFile(filename):
+    accounts = open(filename, 'r', encoding='utf-8').read().split('\n')
+    for account in accounts:
+        if account.startswith('#') or len(account) < 1:
+            continue
+        start(account)
 
 startFromFile(sys.path[0] + '/accounts')
