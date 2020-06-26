c353b68a5f82ed7eadcd58da3f35d5ae90960746
puremdq <puremdq@gmail.com>
2020-06-27 01:51:40 +0800

    Update execPunch.py

--- execPunch.py
+++ execPunch.py
@@ -1,8 +1,9 @@
 import healthPunch.autoPunch as autoPunch
 import asst.asstPunch as asstPunch
 import time
-import os
+import sys
 key = "MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCgZmNj7QvhbpgdqxN7ZCR+r874KZb/qRvlHRieJJREH+i5/hPbpPH5KheEFxoo7nyAkPIcQYPshHvC4UJBe1HrHjdhjFnMA967aebBtioXBOB0qR4ql0DtWA0PrJWtDABeTpPXedqmzMcYIxr1Wq/viIPsjCHRiyRx6mhYqT5P6wIDAQAB"
+
 
 def startFromFile(filename):
     accounts = open(filename, 'r', encoding='utf-8').read().split('\n')
@@ -22,4 +23,4 @@ def start(account):
               " 执行用户:" + account[0] + " 健康打卡 "+str(autoPunch.startPunch('BT7604', 'BT7604')))
 
 
-startFromFile(os.getcwd()+'/accounts')
+startFromFile(sys.path[0] + '/accounts')
