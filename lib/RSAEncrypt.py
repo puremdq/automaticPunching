72a095555a7a2bc9809be105c58848ec1799a0a6
puremdq <puremdq@gmail.com>
2020-06-27 11:42:37 +0800

    把rsa混淆拆到lib下

--- lib/RSAEncrypt.py
+++ lib/RSAEncrypt.py
@@ -1,3 +1,37 @@
+import base64
+import rsa
 
 
+def _str2key(s):
+    # 对字符串解码
+    b_str = base64.b64decode(s)
 
+    if len(b_str) < 162:
+        return False
+
+    hex_str = ''
+
+    # 按位转换成16进制
+    for x in b_str:
+        h = hex(x)[2:]
+        h = h.rjust(2, '0')
+        hex_str += h
+
+    # 找到模数和指数的开头结束位置
+    m_start = 29 * 2
+    e_start = 159 * 2
+    m_len = 128 * 2
+    e_len = 3 * 2
+
+    modulus = hex_str[m_start:m_start + m_len]
+    exponent = hex_str[e_start:e_start + e_len]
+
+    return modulus, exponent
+
+
+def encrypt(s, pubkey):
+    key = _str2key(pubkey)
+    modulus = int(key[0], 16)
+    exponent = int(key[1], 16)
+    pubkey = rsa.PublicKey(modulus, exponent)
+    return base64.b64encode(rsa.encrypt(s.encode(), pubkey)).decode()
